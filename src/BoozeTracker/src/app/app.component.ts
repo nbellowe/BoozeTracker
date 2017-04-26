c3b51d7117457ddf04cb8455f65226434c318edc
Nathan McKenna <namc8997@colorado.edu>
2017-04-26 17:16:23 -0600

    Added age check feature

--- src/BoozeTracker/src/app/app.component.ts
+++ src/BoozeTracker/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -11,12 +11,32 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class BoozeTrackerApp {
     rootPage: any = TabsPage;
 
-    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, alertCtrl: AlertController) {
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             statusBar.styleDefault();
             splashScreen.hide();
+            var alert = alertCtrl.create({
+              title: "Are you 21?",
+              buttons: [
+                {
+                  text:"Yes",
+                  handler: () => {
+                    var alert = alertCtrl.create({
+                      title: "Great! Drink responsibly.",
+                      buttons: ["Ok"]
+                    })
+                    alert.present();
+                  }
+                },
+                {
+                  text: "No",
+                  handler: () => platform.exitApp()
+                }
+              ]
+            })
+            alert.present()
         });
     }
 }
