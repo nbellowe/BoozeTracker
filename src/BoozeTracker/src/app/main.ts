226fbe1fdc88b994f6a8c19fc0424bd41d6e5f2b
Nathan Bellowe <nbellowe@gmail.com>
2017-04-30 03:54:20 -0600

    Trying to make demo site

--- src/BoozeTracker/src/app/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { BoozeTrackerModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(BoozeTrackerModule);
